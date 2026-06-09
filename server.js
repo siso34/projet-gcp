9c48f3ab345e4d6bd9fbc594749a83594d235587
siso34 <serinekh50@gmail.com>
2026-06-09 20:43:52 +0100

    config variables environnement MySQL

--- server.js
+++ server.js
@@ -22,10 +22,11 @@ app.get('/', (req, res) => {
 
 // Connexion à MySQL
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'gcb_db'
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
 
 db.connect((err) => {
